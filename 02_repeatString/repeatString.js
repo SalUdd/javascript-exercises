39173ba9586ee16c9a179583748015b10859f8d9
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2024-08-17 01:37:39 +1000

    Solution found

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    let repeatedString = "";
+    
+    for (let i = 0; i < num; i++) {
+        repeatedString += text;
+    }
+    
+    return repeatedString;
+}
 
-};
 
+
+console.log(repeatString ('goodbye',-1));
 // Do not edit below this line
 module.exports = repeatString;
