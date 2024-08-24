62fa81dc902e690afffbe92158272155a115e4a6
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2024-08-24 13:43:53 +1000

    Solution Found

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celNum) {
+
+  let celTemp = (celNum -32 ) * (5/9);
+  let roundedCelTemp = celTemp.toFixed(1);
+  return +roundedCelTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrNum) {
+  let fahrTemp = (fahrNum * (9/5) + 32 ); 
+  let roundedFahrTemp = fahrTemp.toFixed(1);
+  return +roundedFahrTemp;
 };
+
+console.log(convertToCelsius(-100));
+console.log(convertToFahrenheit(-10));
 
 // Do not edit below this line
 module.exports = {
