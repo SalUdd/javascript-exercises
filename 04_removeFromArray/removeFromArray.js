85fe5bb61ea4b436fb286f91fee5dbf1c498c517
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2024-08-24 12:21:26 +1000

    Solution Found

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const array = [[1, 2, 3, 4], 7, "tacos"];
+
+const removeFromArray = function(value) {
+  if (Array.isArray(value)) {
+    // Remove 3 from any nested array
+    return value.filter(v => v!==7 && v!=="tacos");
+  }
+  return value !==7 && value !=="tacos";
 };
+
+const x = array.map(removeFromArray).filter(value => value.length !== 0);
+
+console.log(`myArray values: ${JSON.stringify(x)}`);
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
