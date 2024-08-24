0f2e0bbc24c9f3ca25bb69eb8fa8a5484eb7cc6a
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2024-08-24 13:08:32 +1000

    Solution Found

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Check for leap year conditions
+    if (year % 400 === 0) {
+        return true; // Leap year
+    } else if (year % 100 === 0) {
+        return false; // Not a leap year
+    } else if (year % 4 === 0) {
+        return true; // Leap year
+    } else {
+        return false; // Not a leap year
+    }
 };
+
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
