3856720ff41fa03cfd7ddce2ea7dee9695fdf606
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2024-10-03 21:51:09 +1000

    solutions found

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    if (n < 0) return "OOPS";
 
+    let prev = 0, current = 1, next;
+
+    for (let i = 2; i <= n; i++) {
+        next = prev + current;
+        prev = current;
+        current = next;
+      }
+    
+      return current;
 };
 
 // Do not edit below this line
